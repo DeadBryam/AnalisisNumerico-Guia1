44c02a8d879042f9441acc97303d5c74147a20df
villa <villa@192.168.0.11>
2018-02-25 19:02:44 -0600

    se termino las GUI

--- src/interfacesUI/PantallaPrincipal.java
+++ src/interfacesUI/PantallaPrincipal.java
@@ -14,8 +14,13 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     /**
      * Creates new form PantallaPrincipal
      */
+    frmAsignacion1 asignacion1 = new frmAsignacion1();
+    frmAsignacion2 asignacion2 = new frmAsignacion2();
+    frmAsignacion3 asignacion3 = new frmAsignacion3();
+    
     public PantallaPrincipal() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -27,6 +32,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         btnAsignacion1 = new javax.swing.JButton();
         btnAsignacion2 = new javax.swing.JButton();
         btnAsignacion3 = new javax.swing.JButton();
@@ -34,71 +40,83 @@ public class PantallaPrincipal extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel1.setPreferredSize(new java.awt.Dimension(400, 320));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnAsignacion1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         btnAsignacion1.setText("Asignacion 1");
+        btnAsignacion1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAsignacion1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnAsignacion1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 180, 110, 25));
 
+        btnAsignacion2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         btnAsignacion2.setText("Asignacion 2");
+        btnAsignacion2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAsignacion2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnAsignacion2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 220, 110, 25));
 
+        btnAsignacion3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         btnAsignacion3.setText("Asignacion 3");
+        btnAsignacion3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAsignacion3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnAsignacion3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 260, 110, 25));
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Análisis numérico 2018");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 360, 22));
 
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Villafuerte Mangandí Bryan Villafuerte");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 87, 360, 22));
 
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Primera tarea de programación");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 53, 360, 22));
 
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Doratt Herrera Luis Enrique");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 360, 22));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(153, 153, 153)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(btnAsignacion1, javax.swing.GroupLayout.DEFAULT_SIZE, 116, Short.MAX_VALUE)
-                            .addComponent(btnAsignacion2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnAsignacion3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(42, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(12, 12, 12)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
-                .addComponent(btnAsignacion1)
-                .addGap(18, 18, 18)
-                .addComponent(btnAsignacion2)
-                .addGap(18, 18, 18)
-                .addComponent(btnAsignacion3)
-                .addGap(57, 57, 57))
-        );
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/back.jpg"))); // NOI18N
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(-140, -70, 600, 440));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 370, 320));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAsignacion1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAsignacion1ActionPerformed
+        asignacion1.show(true);
+        this.show(false);
+    }//GEN-LAST:event_btnAsignacion1ActionPerformed
+
+    private void btnAsignacion2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAsignacion2ActionPerformed
+        asignacion2.show(true);
+        this.show(false);
+    }//GEN-LAST:event_btnAsignacion2ActionPerformed
+
+    private void btnAsignacion3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAsignacion3ActionPerformed
+        asignacion3.show(true);
+        this.show(false);
+    }//GEN-LAST:event_btnAsignacion3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -111,7 +129,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -143,5 +161,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
